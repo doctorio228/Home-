570187bcb7b708bc999aef68a11db6a9bd2c496c
doctorio228 <kukhtin.a@list.ru>
2024-07-21 14:57:14 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+a = 12
+print(a)
+b = 1.5
+print(b)
+course = 'Python'
+print(course)
+c = b/a
+print(c)
+print('Курс:', str(course) + ',','Всего задач:', str(a) +',', 'Затрачено часов:', str(b) + ',', 'среднее время выполнения:', (c),'часа.' )
